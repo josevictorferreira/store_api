1aa41a803b6fddf5db0230584903d5a4918de95a
José Victor Ferreira <josevictorferreira@outlook.com>
2020-02-15 18:25:02 -0300

    Added rswagger and create first specs for the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates_presence_of :name, :price
+  validates :price, numericality: { greater_than: 0 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid when name and price are present' do
+    user = Product.new(name: 'Product Test 1', price: 10.0)
+    expect(user).to be_valid
+  end
+
+  it 'should be invalid when name is not present' do
+    user = Product.new(price: 10.0)
+    expect(user).to be_invalid
+  end
+
+  it 'should be invalid when price is not present' do
+    user = Product.new(name: 'Product Test 1')
+    expect(user).to be_invalid
+  end
+
+  it 'should be invalid when name is an empty value' do
+    user = Product.new(name: '', price: 10.0)
+    expect(user).to be_invalid
+  end
+
+  it 'should be invalid when price is a negative number' do
+    user = Product.new(name: 'Product Test 1', price: -10.0)
+    expect(user).to be_invalid
+  end
 end
